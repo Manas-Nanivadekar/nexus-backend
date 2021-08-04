a06851b87f0ee5f66dffc8bc35f45cb5b30fa25d
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2021-08-04 09:22:42 +0530

    payee api add

---
+++ src/routes/payee/confirmFrom.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { confirmProcess } = require("../../db/payee/confirmProcess");
+
+const router = express.Router();
+
+router.post("/v1/confirmfrom", async (req, res) => {
+  const data = req.body;
+
+  const hash = await confirmProcess(data.decision);
+
+  res.status(200).json({
+    sucess: true,
+    hash: hash,
+    message: "Confirmation received from destination gateway",
+  });
+});
+
+module.exports = router;
