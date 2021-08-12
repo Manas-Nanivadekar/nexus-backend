ea448c7a1d9411907458883b455c92424ad58d05
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2021-08-12 22:29:31 +0530

    params update

--- src/db/quote/deleteQuote.js
+++ src/db/quote/deleteQuote.js
@@ -1,7 +1,7 @@
 const connect = require("../connection");
 const { Keyring } = require("@polkadot/keyring");
 
-const deleteRate = async (uuid) => {
+const deleteRate = async (source_currency, dest_currency, uuid) => {
   const api = await connect();
   // Constuct the keyring after the API (crypto has an async init)
   const keyring = new Keyring({ type: "sr25519" });
@@ -9,7 +9,11 @@ const deleteRate = async (uuid) => {
   // Add Alice to our keyring with a hard-deived path (empty phrase, so uses dev)
   const alice = keyring.addFromUri("//Alice");
 
-  const rate = api.tx.nexusApiQuote.deleteRate(uuid);
+  const rate = api.tx.nexusApiQuote.deleteRate(
+    source_currency,
+    dest_currency,
+    uuid
+  );
 
   const hash = await rate.signAndSend(alice);
 
